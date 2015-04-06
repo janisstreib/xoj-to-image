6b10a46124b5a8efc203f8d5d1f6acca023a8557
Janis Streib <janis@dogcraft.de>
2015-04-06 19:54:51 +0200

    added all other colors

--- src/de/yellowant/janis/xojtopdf/xournalelements/Color.java
+++ src/de/yellowant/janis/xojtopdf/xournalelements/Color.java
@@ -2,7 +2,11 @@ package de.yellowant.janis.xojtopdf.xournalelements;
 
 public enum Color {
 	BLACK(java.awt.Color.BLACK), YELLOW(java.awt.Color.YELLOW), ORANGE(
-			java.awt.Color.ORANGE);
+			java.awt.Color.ORANGE), RED(java.awt.Color.RED), BLUE(
+			java.awt.Color.BLUE), WHITE(java.awt.Color.WHITE), GRAY(
+			java.awt.Color.GRAY), LIGHTBLUE(java.awt.Color.CYAN), LIGHTGREEN(
+			java.awt.Color.decode("0x3dff00")), MAGENTA(java.awt.Color.MAGENTA), GREEN(
+			java.awt.Color.decode("0x1a7100"));
 	
 	private java.awt.Color awtColor;
 
@@ -23,6 +27,22 @@ public enum Color {
 			return YELLOW;
 		case "orange":
 			return ORANGE;
+		case "red":
+			return RED;
+		case "gray":
+			return GRAY;
+		case "lightblue":
+			return LIGHTBLUE;
+		case "lightgreen":
+			return LIGHTGREEN;
+		case "magenta":
+			return MAGENTA;
+		case "white":
+			return WHITE;
+		case "green":
+			return GREEN;
+		case "blue":
+			return BLUE;
 		default:
 			return BLACK;
 		}
