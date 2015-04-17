a69da934b578e2da20366c940c3f782412859d42
Janis Streib <janis@dogcraft.de>
2015-04-17 12:54:49 +0200

    UPD: suffix sepcific renderer selction and refurbished cmd arguments

--- src/de/yellowant/xojtoimage/InteractiveUserInterface.java
+++ src/de/yellowant/xojtoimage/InteractiveUserInterface.java
@@ -1,22 +1,41 @@
 package de.yellowant.xojtoimage;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 import de.yellowant.xojtoimage.renderer.PageCanvas;
+import de.yellowant.xojtoimage.renderer.Renderer;
+import de.yellowant.xojtoimage.renderer.pdf.PdfRenderer;
 
 public class InteractiveUserInterface {
-	private static final String USAGE = "Uasge:\njava -jar xojtoimage.jar <Xournal file> <format> <scale factor> <ouput file prefix>";
+	private static final String USAGE = "Uasge:\njava -jar xojtoimage.jar <Xournal file> <destination file> [<scale factor>]";
 
 	public static void main(String[] args) {
 		try {
-			XOJToImage xoj = new XOJToImage(new File(args[0]), new PageCanvas(
-					Float.parseFloat(args[2])));
-			xoj.exportAll(args[3], args[1]);
+			Renderer r = null;
+			String file = args[1].toLowerCase();
+			if (file.endsWith(".pdf")) {
+				r = new PdfRenderer();
+			} else if (file.endsWith(".png") || file.endsWith(".gif")) {
+				int factor = 1;
+				if (args.length == 3) {
+					factor = Integer.parseInt(args[2]);
+				}
+				r = new PageCanvas(factor);
+			}
+			if (r == null) {
+				System.err.println("No known output format supplied!");
+			}
+			XOJToImage xoj = new XOJToImage(new File(args[0]), r);
+			String[] fileParts = args[1].split(Pattern.quote("."));
+			xoj.exportAll(fileParts[fileParts.length - 2],
+					fileParts[fileParts.length - 1]);
 			System.out.println("Done. Processed "
 					+ xoj.getXournal().getPages().size() + " pages.");
 		} catch (Exception e) {
-			System.err.println("Error, please check your input! ("
-					+ e.getMessage() + ")");
+			System.err.println("Please check your input! (" + e.getMessage()
+					+ ")");
+			e.printStackTrace();
 			System.out.println(USAGE);
 		}
 	}
